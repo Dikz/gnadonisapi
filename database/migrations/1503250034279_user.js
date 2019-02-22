c4b8c855599a969f5bac136f8dcfcf5359e57b60
Dikz <cicerogabriel12@hotmail.com>
2019-02-22 02:32:01 -0300

    Novos campos na migration user

--- database/migrations/1503250034279_user.js
+++ database/migrations/1503250034279_user.js
@@ -4,17 +4,19 @@
 const Schema = use('Schema')
 
 class UserSchema extends Schema {
-  up () {
+  up() {
     this.create('users', (table) => {
       table.increments()
       table.string('username', 80).notNullable().unique()
       table.string('email', 254).notNullable().unique()
       table.string('password', 60).notNullable()
+      table.string('token')
+      table.timestamp('token_created_at')
       table.timestamps()
     })
   }
 
-  down () {
+  down() {
     this.drop('users')
   }
 }
