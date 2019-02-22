6ee72241dc162c740c5ef531c0fda5704cbb2998
Dikz <cicerogabriel12@hotmail.com>
2019-02-22 15:35:27 -0300

    Rota /files

--- start/routes.js
+++ start/routes.js
@@ -7,3 +7,5 @@ Route.post('sessions', 'SessionController.store')
 
 Route.post('forgot', 'ForgotPasswordController.store')
 Route.put('forgot', 'ForgotPasswordController.update')
+
+Route.post('/files', 'FileController.store')
