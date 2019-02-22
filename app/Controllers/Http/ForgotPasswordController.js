71232f989045584208c4c7fb1e322d3c71e0401d
Dikz <cicerogabriel12@hotmail.com>
2019-02-22 03:28:16 -0300

    Metodo update() : Alterar senha

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const crypto = require('crypto')
+const moment = require('moment')
 const User = use('App/Models/User')
 const Mail = use('Mail')
 
@@ -37,6 +38,47 @@ class ForgotPasswordController {
           message: 'Algo não deu certo, esse e-mail existe?'
         }
       })
+    }
+  }
+
+  async update({
+    request,
+    response
+  }) {
+    try {
+      const {
+        token,
+        password
+      } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({
+            error: {
+              message: 'O token de recuperação está expirado!'
+            }
+          })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({
+          error: {
+            message: 'Algo deu errado ao resetar sua senha'
+          }
+        })
     }
   }
 }
