e720da96e88fd886e0eb08259850bfbf10d09ed9
Dikz <cicerogabriel12@hotmail.com>
2019-02-22 16:08:01 -0300

    Rota para visualização de arquivo

--- start/routes.js
+++ start/routes.js
@@ -8,4 +8,5 @@ Route.post('sessions', 'SessionController.store')
 Route.post('forgot', 'ForgotPasswordController.store')
 Route.put('forgot', 'ForgotPasswordController.update')
 
+Route.get('/files/:id', 'FileController.show')
 Route.post('/files', 'FileController.store')
