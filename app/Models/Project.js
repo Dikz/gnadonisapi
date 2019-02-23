64ab698ba40d52fb8a7a5de264cc3e2d6ed61943
Dikz <cicerogabriel12@hotmail.com>
2019-02-23 17:14:46 -0300

    Relacionamentos entre Models

--- app/Models/Project.js
+++ app/Models/Project.js
@@ -1,9 +1,15 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Project extends Model {
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  tasks() {
+    return this.hasMany('App/Model/Task')
+  }
 }
 
 module.exports = Project
--- app/Models/Task.js
+++ app/Models/Task.js
@@ -1,9 +1,19 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Task extends Model {
+  project() {
+    return this.belongsTo('App/Models/Project')
+  }
+
+  user() {
+    return this.belongsTo('App/Models/User')
+  }
+
+  file() {
+    return this.belongsTo('App/Models/File')
+  }
 }
 
 module.exports = Task
