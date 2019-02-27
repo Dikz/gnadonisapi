e609fb750dcabc89466bf0040865a747e4c4c5fc
Dikz <cicerogabriel12@hotmail.com>
2019-02-26 21:05:36 -0300

    Rota de Tasks

--- start/routes.js
+++ start/routes.js
@@ -12,4 +12,5 @@ Route.get('/files/:id', 'FileController.show')
 Route.group(() => {
   Route.post('/files', 'FileController.store')
   Route.resource('projects', 'ProjectController').apiOnly()
+  Route.resource('projects.tasks', 'TaskController').apiOnly()
 }).middleware(['auth'])
